ad5880f5ed532f7990481c259b2b75142cd821f9
JeyLouis <imjerrylouis@gmail.com>
2021-04-02 12:35:50 -0400

    answer2

--- Beginner/capSentence/index-START.js
+++ Beginner/capSentence/index-START.js
@@ -1,7 +1,7 @@
 /* CHALLENGE
-Given a sentence containing two or more words, 
+Given a sentence containing two or more words,
 return the equivalent of the sentence when capitalised. E.g
-  capSentence('the tales of scotch!') // would return 'The Tales Of Scotch!' 
+  capSentence('the tales of scotch!') // would return 'The Tales Of Scotch!'
 */
 
 
@@ -9,6 +9,11 @@ return the equivalent of the sentence when capitalised. E.g
 
 function capSentence(text) {
    // Code goes here
+   let copy = text.toLowerCase().split(" ")
+   let newArr = copy.map( co => {
+     return co.charAt(0).toUpperCase() + co.slice(1)
+   })
+   return newArr.join(" ")
 }
 
 
